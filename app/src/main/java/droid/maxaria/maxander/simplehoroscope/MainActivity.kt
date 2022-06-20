2d6094ba0c4c35d314cdc73275d0074df2f6b738
maxander3 <maxander234432@gmail.com>
2022-06-20 18:42:52 +0300

    Initial commit

---
+++ app/src/main/java/droid/maxaria/maxander/simplehoroscope/MainActivity.kt
@@ -0,0 +1,11 @@
+package droid.maxaria.maxander.simplehoroscope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
